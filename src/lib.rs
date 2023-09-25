996cdca78740d6c2ec0e5641f4c63e7fb881ea57
Amando Z <avhzanderigo@gmail.com>
2023-09-25 22:29:32 +0200

    Initial commit.

---
+++ src/lib.rs
@@ -0,0 +1,59 @@
+//! A simplex complex number implementation for Rust.
+
+use std::ops::{Add, Div, Mul, Sub};
+
+const IMAGINARY_UNIT: Complex = Complex { re: 0.0, im: 1.0 };
+
+struct Complex {
+    re: f64,
+    im: f64,
+}
+
+impl Complex {
+    fn new(re: f64, im: f64) -> Complex {
+        Complex { re: re, im: im }
+    }
+
+    fn i() -> Complex {
+        IMAGINARY_UNIT
+    }
+}
+
+impl Add for Complex {
+    type Output = Complex;
+
+    fn add(self, other: Complex) -> Complex {
+        Complex::new(self.re + other.re, self.im + other.im)
+    }
+}
+
+impl Sub for Complex {
+    type Output = Complex;
+
+    fn sub(self, other: Complex) -> Complex {
+        Complex::new(self.re - other.re, self.im - other.im)
+    }
+}
+
+impl Mul for Complex {
+    type Output = Complex;
+
+    fn mul(self, other: Complex) -> Complex {
+        Complex::new(
+            self.re * other.re - self.im * other.im,
+            self.re * other.im + self.im * other.re,
+        )
+    }
+}
+
+impl Div for Complex {
+    type Output = Complex;
+
+    fn div(self, other: Complex) -> Complex {
+        let denom = other.re * other.re + other.im * other.im;
+        Complex::new(
+            (self.re * other.re + self.im * other.im) / denom,
+            (self.im * other.re - self.re * other.im) / denom,
+        )
+    }
+}
