29ac9e5a6651a8bb1b8b713edc4b3a94b3a9d746
Amando Z <avhzanderigo@gmail.com>
2023-09-26 13:22:53 +0200

    Common functions.

--- src/lib.rs
+++ src/lib.rs
@@ -1,60 +1,104 @@
 //! A simplex complex number implementation for Rust.
 
-use std::ops::{Add, Div, Mul, Sub};
+mod arithmetic;
+mod polar;
+mod trigonometric;
 
-const IMAGINARY_UNIT: Complex = Complex { re: 0.0, im: 1.0 };
-
-struct Complex {
-    re: f64,
-    im: f64,
+#[derive(Debug, Clone, Copy, Eq, PartialEq)]
+pub struct Complex<F>
+where
+    F: num::Float,
+{
+    pub re: F,
+    pub im: F,
 }
 
-impl Complex {
-    fn new(re: f64, im: f64) -> Complex {
-        Complex { re, im }
-    }
-
-    fn i() -> Complex {
-        IMAGINARY_UNIT
-    }
-}
-
-impl Add for Complex {
-    type Output = Complex;
-
-    fn add(self, other: Complex) -> Complex {
-        Complex::new(self.re + other.re, self.im + other.im)
+impl<F> Default for Complex<F>
+where
+    F: num::Float,
+{
+    fn default() -> Self {
+        Self {
+            re: F::zero(),
+            im: F::zero(),
+        }
     }
 }
 
-impl Sub for Complex {
-    type Output = Complex;
-
-    fn sub(self, other: Complex) -> Complex {
-        Complex::new(self.re - other.re, self.im - other.im)
+impl<F> Complex<F>
+where
+    F: num::Float,
+{
+    pub fn new(re: F, im: F) -> Self {
+        Self { re, im }
     }
-}
 
-impl Mul for Complex {
-    type Output = Complex;
-
-    fn mul(self, other: Complex) -> Complex {
-        Complex::new(
-            self.re * other.re - self.im * other.im,
-            self.re * other.im + self.im * other.re,
-        )
+    pub fn i() -> Self {
+        Self {
+            re: F::zero(),
+            im: F::one(),
+        }
     }
-}
 
-impl Div for Complex {
-    type Output = Complex;
+    pub fn origin() -> Self {
+        Self {
+            re: F::zero(),
+            im: F::zero(),
+        }
+    }
 
-    fn div(self, other: Complex) -> Complex {
-        let denom = other.re * other.re + other.im * other.im;
+    pub fn from_polar(r: F, theta: F) -> Self {
+        Self {
+            re: r * theta.cos(),
+            im: r * theta.sin(),
+        }
+    }
 
-        Complex::new(
-            (self.re * other.re + self.im * other.im) / denom,
-            (self.im * other.re - self.re * other.im) / denom,
+    pub fn re(&self) -> F {
+        self.re
+    }
+
+    pub fn im(&self) -> F {
+        self.im
+    }
+
+    pub fn norm(&self) -> F {
+        self.re * self.re + self.im * self.im
+    }
+
+    pub fn arg(&self) -> F {
+        self.im.atan2(self.re)
+    }
+
+    pub fn conj(&self) -> Self {
+        Self {
+            re: self.re,
+            im: -self.im,
+        }
+    }
+
+    pub fn polar(&self) -> (F, F) {
+        (self.norm().sqrt(), self.arg())
+    }
+
+    pub fn exp(&self) -> Self {
+        Self::from_polar(F::exp(self.re), self.im)
+    }
+
+    pub fn ln(&self) -> Self {
+        Self::new(self.norm().ln(), self.arg())
+    }
+
+    pub fn sqrt(&self) -> Self {
+        let (r, theta) = self.polar();
+
+        Self::from_polar(r.sqrt(), theta / F::from(2).unwrap())
+    }
+
+    pub fn sin(&self) -> Self {
+        Self::new(
+            self.re.sin() * self.im.cosh(),
+            self.re.cos() * self.im.sinh(),
         )
     }
 }
